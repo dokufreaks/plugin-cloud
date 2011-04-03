7f10b0170254a6f9787db97141cb24d377363b2e
Matthias Schulte <post@lupo49.de>
2011-04-03 12:59:36 +0200

    eo: added esperanto language file

---
+++ lang/eo/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Esperanto language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     ka2pink
+ */
+ 
+// for the configuration manager
+$lang['minimum_word_length']      = 'Minimuma longo de vortoj por nubo';
+$lang['tag_blacklist']            = 'Etikedoj, kiuj ne devas aperi en la nubo (separitaj per komo)';
+
+//Setup VIM: ex: et ts=2 :
