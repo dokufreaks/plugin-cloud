d496e6e1db9df33530979fb22a76acd7eb22fbb8
Myeongjin <aranet100@gmail.com>
2013-08-20 02:20:34 +0200

    translation update

--- lang/ko/settings.php
+++ lang/ko/settings.php
@@ -7,4 +7,4 @@
  */
 $lang['minimum_word_length']   = '구름에서 낱말의 최소 낱말 길이';
 $lang['tag_blacklist']         = '문서에 보여주지 않아야 하는 태그 (쉼표로 구분)';
-$lang['list_tags_of_subns']    = '또한 지정된 이름공간의 하위 이름공간에서의 태그 목록';
+$lang['list_tags_of_subns']    = '지정된 이름공간의 하위 이름공간에서 태그도 나열';
