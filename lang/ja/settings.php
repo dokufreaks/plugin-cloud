e3f6dd34e73925fed4e80d83fba040a1eae77679
Hideaki SAWADA <chuno@live.jp>
2013-09-15 19:10:04 +0200

    translation update

---
+++ lang/ja/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['minimum_word_length']   = 'クラウド内の単語の最小ワード長';
+$lang['tag_blacklist']         = 'ページ表示から除外するタグ（カンマ区切り）';
+$lang['list_tags_of_subns']    = '指定した名前空間のサブ名前空間のタグも一覧表示する';
