5152297a4a509dbb5db7a58027b15035936c9636
NicolasFriedli <nicolas@theologique.ch>
2013-12-07 10:26:07 +0100

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author NicolasFriedli <nicolas@theologique.ch>
+ */
+$lang['minimum_word_length']   = 'Longueur de mot minimale dans le cloud';
+$lang['tag_blacklist']         = 'Tags ne devant pas apparaître sur les pages (séparés par des virgules)';
+$lang['list_tags_of_subns']    = 'Lister également les tags des sous-espaces de nom d\'un espace de nom donné';
