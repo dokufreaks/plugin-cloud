d1d30259e79b60eae7b15959ac43d84907b11f29
Søren Birk <soer9648@eucl.dk>
2014-12-10 10:20:06 +0100

    translation update

---
+++ lang/da/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Søren Birk <soer9648@eucl.dk>
+ */
+$lang['minimum_word_length']   = 'Minimum ordlængde for ord i skyen';
+$lang['tag_blacklist']         = 'Tags, som ikke skal vises på sider (kommasepareret)';
+$lang['list_tags_of_subns']    = 'List også tags i under-navnerum for et specificeret navnerum';
