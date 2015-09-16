ed220128e8c481982bba9c7aa62031ff760abbd4
Pavel Kochman <pavel.kochman@certicon.cz>
2015-09-16 13:45:44 +0200

    added option to specify which word shouldn't appear in the cloud

--- lang/cs/settings.php
+++ lang/cs/settings.php
@@ -6,5 +6,6 @@
  * @author Jaroslav Lichtblau <jlichtblau@seznam.cz>
  */
 $lang['minimum_word_length']   = 'Minimální délka slov pro zobrazení v oblaku';
+$lang['word_blacklist']        = 'Slova, která nemají být na stránce zobrazeny (oddělené čárkou)';
 $lang['tag_blacklist']         = 'Tagy, které nemají být na stránce zobrazeny (oddělené čárkou)';
 $lang['list_tags_of_subns']    = 'Vypisovat také tagy v podprostorech vybraného jmenného prostoru';
