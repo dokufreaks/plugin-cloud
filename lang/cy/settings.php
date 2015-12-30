6758c2dec4ee69cd8a0291c4c84e0ea766db1dd7
Alan Davies <ben.brynsadler@gmail.com>
2015-12-30 16:20:09 +0100

    translation update

---
+++ lang/cy/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Alan Davies <ben.brynsadler@gmail.com>
+ */
+$lang['minimum_word_length']   = 'Isafswm hyd gair mewn geiriau yn y cwmwl';
+$lang['tag_blacklist']         = 'Tagiau, na ddylai cael eu harddangos mewn tudalennau (Gwahanu gan goma)';
+$lang['list_tags_of_subns']    = 'Rhestru tagiau hefyd mewn subnamespaces o namespace penodol';
