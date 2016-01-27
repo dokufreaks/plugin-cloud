29b82c462b56b75922c8a40c5704a7ac917a4adf
Daniel Raknes <rada@jbv.no>
2016-01-27 14:05:04 +0100

    translation update

---
+++ lang/no/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Daniel Raknes <rada@jbv.no>
+ */
+$lang['minimum_word_length']   = 'Minimum ordlengde for ord i ordskyen ';
+$lang['tag_blacklist']         = ' Ord som ikke skal vises i ordskyen (kommaseparert)';
+$lang['list_tags_of_subns']    = 'Listen gjelder også undernavnerom til spesifisert navnerom';
