49a800faf7e0965f929052d574cf6efe6083db4e
Myeongjin <aranet100@gmail.com>
2016-07-27 12:40:30 +0200

    translation update

--- lang/ko/settings.php
+++ lang/ko/settings.php
@@ -2,9 +2,9 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Myeongjin <aranet100@gmail.com>
  */
 $lang['minimum_word_length']   = '구름에서 낱말의 최소 낱말 길이';
-$lang['tag_blacklist']         = '문서에 보여주지 않아야 하는 태그 (쉼표로 구분)';
+$lang['tag_blacklist']         = '문서에 표시되지 않아야 하는 태그 (쉼표로 구분)';
 $lang['list_tags_of_subns']    = '지정된 이름공간의 하위 이름공간에서 태그도 나열';
