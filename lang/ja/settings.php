5f396d57edc2961578041da57b087d1deffea66b
Hideaki SAWADA <chuno@live.jp>
2017-08-01 09:05:21 +0200

    translation update

--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -2,9 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Hideaki SAWADA <chuno@live.jp>
  */
 $lang['minimum_word_length']   = 'クラウド内の単語の最小ワード長';
+$lang['search_blacklist']      = 'ページ表示から除外する検索語（カンマ区切り）';
+$lang['word_blacklist']        = 'ページ表示から除外する単語（カンマ区切り）';
 $lang['tag_blacklist']         = 'ページ表示から除外するタグ（カンマ区切り）';
 $lang['list_tags_of_subns']    = '指定した名前空間のサブ名前空間のタグも一覧表示する';
