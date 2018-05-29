09a0a70ba4e1b2d25e1452136c6519684978ce24
Harriet Neitz <h.e.neitz@utwente.nl>
2018-05-29 13:15:11 +0200

    translation update

--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -2,9 +2,12 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Harriet Neitz <h.e.neitz@utwente.nl>
  * @author Mark Prins <mprins@users.sf.net>
  */
-$lang['minimum_word_length']   = 'Minimum woord lengte voor opname van een woord in de wolk';
-$lang['tag_blacklist']         = 'Labels welke niet getoond dienen te worden op pagina\'s (komma gescheiden)';
-$lang['list_tags_of_subns']    = 'Ook labels uit een sub-naamruimte van een naamruimte tonen';
+$lang['minimum_word_length']   = 'Minimum woordlengte voor opname van een woord in de wolk';
+$lang['search_blacklist']      = 'Zoek woorden die niet op pagina\'s weergegeven moeten worden (Gescheiden door komma\'s)';
+$lang['word_blacklist']        = 'Woorden die niet op pagina\'s weergegeven moeten worden (Gescheiden door komma\'s)';
+$lang['tag_blacklist']         = 'Labels die niet getoond dienen te worden op pagina\'s (komma gescheiden)';
+$lang['list_tags_of_subns']    = 'Ook labels uit een sub-namespace van een namespace tonen';
