fb3d3433f2b579a4186d67b56bdbdf824373e264
Jacob Palm <jacobpalmdk@icloud.com>
2020-05-22 00:30:35 +0200

    translation update

--- lang/da/settings.php
+++ lang/da/settings.php
@@ -2,9 +2,12 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Jacob Palm <jacobpalmdk@icloud.com>
  * @author Søren Birk <soer9648@eucl.dk>
  */
 $lang['minimum_word_length']   = 'Minimum ordlængde for ord i skyen';
+$lang['search_blacklist']      = 'Søgeord der ikke skal vises på sider (kommasepareret)';
+$lang['word_blacklist']        = 'Ord der ikke skal vises på sider (kommasepareret)';
 $lang['tag_blacklist']         = 'Tags, som ikke skal vises på sider (kommasepareret)';
 $lang['list_tags_of_subns']    = 'List også tags i under-navnerum for et specificeret navnerum';
