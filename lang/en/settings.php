3656336d6322023f362b02a708b2aae3d2bd9571
Tosha <tosha.wolfgram@live.fr>
2024-03-03 02:00:45 +0100

    translation update

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,16 +1,28 @@
 <?php
+
 /**
  * English language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Matthias Schulte <dokuwiki@lupo49.de>
+ *
+ * @author Tosha <tosha.wolfgram@live.fr>
+ * @author Matthias Schulte <dokuwiki@lupo49.de>
  */
- 
-// for the configuration manager
-$lang['minimum_word_length']      = 'Minimum word-length for words in the cloud';
-$lang['search_blacklist']         = 'Search words, which shouldn\'t be displayed on pages (Comma seperated)';
-$lang['word_blacklist']           = 'Words, which shouldn\'t be displayed on pages (Comma seperated)';
-$lang['tag_blacklist']            = 'Tags, which shouldn\'t be displayed on pages (Comma seperated)';
-$lang['list_tags_of_subns']       = 'List also tags in subnamespaces of a specified namespace';
-
-//Setup VIM: ex: et ts=2 :
+$lang['minimum_word_length']   = 'https://bankomat.biz
+Good day I am so delighted I found your web site, I really found you by accident, while I 
+was researching on Yahoo for something else, 
+Anyways I am here now and would just like to say cheers for a remarkable post and a all round thrilling blog (I also love 
+the theme/design), I don\'t have time to read through it all at 
+the minute but I have book-marked it and also included your RSS feeds, so when I have 
+time I will be back to read a great deal more, Please do keep up the awesome job.';
+$lang['search_blacklist']      = 'Search words, which shouldn\'t be displayed on pages (Comma seperated)';
+$lang['word_blacklist']        = 'https://bankomat.biz
+Good day I am so delighted I found your web site, I really found you by accident, while I 
+was researching on Yahoo for something else, 
+Anyways I am here now and would just like to say 
+cheers for a remarkable post and a all round thrilling blog (I 
+also love the theme/design), I don\'t have time to read through it all at the minute but I have book-marked it 
+and also included your RSS feeds, so when I 
+have time I will be back to read a great deal more, Please do keep up the awesome job.';
+$lang['tag_blacklist']         = '10';
+$lang['list_tags_of_subns']    = '11';
