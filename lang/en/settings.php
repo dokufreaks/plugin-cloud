d2b62d85a0c8f36aed2b850662a0e2d03d55d757
Jonathon <jonathonbardsley@yahoo.co.in>
2024-03-06 03:05:43 +0100

    translation update

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,16 +1,23 @@
 <?php
+
 /**
  * English language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Matthias Schulte <dokuwiki@lupo49.de>
+ *
+ * @author Jonathon <jonathonbardsley@yahoo.co.in>
+ * @author Matthias Schulte <dokuwiki@lupo49.de>
  */
- 
-// for the configuration manager
-$lang['minimum_word_length']      = 'Minimum word-length for words in the cloud';
-$lang['search_blacklist']         = 'Search words, which shouldn\'t be displayed on pages (Comma seperated)';
-$lang['word_blacklist']           = 'Words, which shouldn\'t be displayed on pages (Comma seperated)';
-$lang['tag_blacklist']            = 'Tags, which shouldn\'t be displayed on pages (Comma seperated)';
-$lang['list_tags_of_subns']       = 'List also tags in subnamespaces of a specified namespace';
+$lang['minimum_word_length']   = 'http://vclub-cc.ru
+Hi there! This post could not be written any better!
 
-//Setup VIM: ex: et ts=2 :
+Reading through this post reminds me of my previous room mate!
+He always kept chatting about this. I will forward this page to him.
+Pretty sure he will have a good read. Thank you for sharing!';
+$lang['search_blacklist']      = 'Search words, which shouldn\'t be displayed on pages (Comma seperated)';
+$lang['word_blacklist']        = 'http://vclub-cc.ru
+Hi there! This post could not be written any better! Reading through this post reminds me of my previous room mate!
+He always kept chatting about this. I will forward this page to 
+him. Pretty sure he will have a good read. Thank you for sharing!';
+$lang['tag_blacklist']         = '22';
+$lang['list_tags_of_subns']    = '21';
