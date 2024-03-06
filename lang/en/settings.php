d4fed4b28d199eb71207af14c0aa3cf253c7ae00
Sadye <sadyemowery@uol.com.br>
2024-03-06 01:55:04 +0100

    translation update

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,16 +1,30 @@
 <?php
+
 /**
  * English language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Matthias Schulte <dokuwiki@lupo49.de>
+ *
+ * @author Sadye <sadyemowery@uol.com.br>
+ * @author Matthias Schulte <dokuwiki@lupo49.de>
  */
- 
-// for the configuration manager
-$lang['minimum_word_length']      = 'Minimum word-length for words in the cloud';
-$lang['search_blacklist']         = 'Search words, which shouldn\'t be displayed on pages (Comma seperated)';
-$lang['word_blacklist']           = 'Words, which shouldn\'t be displayed on pages (Comma seperated)';
-$lang['tag_blacklist']            = 'Tags, which shouldn\'t be displayed on pages (Comma seperated)';
-$lang['list_tags_of_subns']       = 'List also tags in subnamespaces of a specified namespace';
-
-//Setup VIM: ex: et ts=2 :
+$lang['minimum_word_length']   = 'https://donald-cvv.at
+Hi I am so grateful I found your blog page, 
+I really found you by error, while I was looking on Aol for something 
+else, Regardless I am here now and would just like to say kudos for a incredible post and a 
+all round thrilling blog (I also love the theme/design),
+I don’t have time to read through it all at the moment but I have 
+bookmarked it and also added your RSS feeds, so when I have time I 
+will be back to read a great deal more, Please do keep up the superb 
+jo.';
+$lang['search_blacklist']      = 'Search words, which shouldn\'t be displayed on pages (Comma seperated)';
+$lang['word_blacklist']        = 'https://donald-cvv.at
+Hi I am so grateful I found your blog page,
+I really found you by error, while I was looking on Aol for something else, Regardless I am here now and would just like to say 
+kudos for a incredible post and a all round thrilling blog 
+(I also love the theme/design), I don’t have time to read through it all at the moment but I have bookmarked it 
+and also added your RSS feeds, so when I have time I will be back 
+to read a great deal more, Please do keep up 
+the superb jo.';
+$lang['tag_blacklist']         = '20';
+$lang['list_tags_of_subns']    = '15';
