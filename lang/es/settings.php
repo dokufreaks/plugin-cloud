b5a6b53d6ef88464e3a4781b47fb67eb39d64419
Raul Luna <superman_ha_muerto@yahoo.com>
2024-03-09 13:50:36 +0100

    translation update

---
+++ lang/es/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Raul Luna <superman_ha_muerto@yahoo.com>
+ */
+$lang['minimum_word_length']   = 'Longitud mínima de las palabras en la nube';
+$lang['search_blacklist']      = 'Palabras de búsqueda, que no deberían aparecer en páginas (separado por comas)';
+$lang['word_blacklist']        = 'Palabras, que no deberían aparecer en páginas (separado por comas)';
+$lang['tag_blacklist']         = 'Etiquetas, que no deberían aparecer en páginas (separado por comas)';
+$lang['list_tags_of_subns']    = 'Listar también etiquetas en subespacios de nombres de un espacio de nombres concreto';
